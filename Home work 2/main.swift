6b86a29b5306ac4556e8753029fa2e11ce688bf7
AfanasevAlexey <ya.alexey.27@mail.ru>
2019-03-06 20:29:44 +0900

    добавил домашнее задание №3

---
+++ Home work 2/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Home work 2
+//
+//  Created by Алексей on 05/03/2019.
+//  Copyright © 2019 Alexey. All rights reserved.
+//
+//3 задание
+import Foundation
+var array = Array (1...100)
+print(array)
+
+
+
+
+
