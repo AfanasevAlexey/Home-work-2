7ad6dc6fe657968122a2f3dd032c69e5621ec40b
AfanasevAlexey <ya.alexey.27@mail.ru>
2019-03-09 08:14:29 +0900

    сделал квадратное уравнение

--- Home Work 1/Home Work 1/main.swift
+++ Home Work 1/Home Work 1/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Hello, print a: ")
+var a = Double(readLine()!)
 
+print("Now print b: ")
+var b = Double(readLine()!)
+
+print("Now print c: ")
+var c = Double (readLine()!)
+
+print("Now we find the roots of the equation \(a!)x^2+\(b!)x+\(c!)=0")
+var d = b!*b!-4*a!*c!
+
+if d > 0 {
+    print("D=\(d). D>0, we have two roots.")
+    
+    print("X1: \((-1*b!+sqrt(d))/(2*a!)), X2: \((-1*b!-sqrt(d))/(2*a!))")
+} else if d == 0{
+    
+    print("D=\(d). D=0, we have one root.")
+    print("X: \((-1*b!)/(2*a!))")
+    
+} else if d < 0{
+    print("D=\(d). D<0 we dont have roots. ")
+}
