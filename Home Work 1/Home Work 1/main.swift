7ce096d2f2252cedeb2dbc2a41014e01a3fc83e8
AfanasevAlexey <ya.alexey.27@mail.ru>
2019-03-09 08:12:27 +0900

    сделал первое задание к первому уроку

---
+++ Home Work 1/Home Work 1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Home Work 1
+//
+//  Created by Алексей on 09/03/2019.
+//  Copyright © 2019 Alexey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
